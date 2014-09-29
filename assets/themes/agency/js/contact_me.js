561a7893ff0a036fdd5e5f4272abc3d77bd2f6fa
Udit Desai <desaiuditd@gmail.com>
2014-09-29 10:20:59 +0530

    Contact Form Success Message

--- assets/themes/agency/js/contact_me.js
+++ assets/themes/agency/js/contact_me.js
@@ -32,7 +32,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Thanks for contacting. Your message has been sent. You will hear from me soon!</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
